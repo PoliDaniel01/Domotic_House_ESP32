3ccd6367024a6dd80dad98c6846be2f9bc3d78ae
PoliDaniel01 <113338365+PoliDaniel01@users.noreply.github.com>
2025-06-27 15:35:39 +0200

    Update master.py

--- master/master.py
+++ master/master.py
@@ -3,7 +3,7 @@ from machine import Pin, SoftI2C
 import network
 import time
 import json
-from lib.umqtt import simple, robust
+from umqtt.simple import MQTTClient
 from lib.st7789 import    #put there the missing library
 
 # === CONFIGURATION ===
